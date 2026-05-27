424313dc4482fe1f22632433831cf66cba055cc6
Jaimeml95 <jaimemorolopez@gmail.com>
2026-05-27 20:30:47 +0200

    Fix: Correcciones menores en el código

--- app/src/main/java/com/jaimemoro/cornermanbox/data/entities/Entrenamiento.java
+++ app/src/main/java/com/jaimemoro/cornermanbox/data/entities/Entrenamiento.java
@@ -1,6 +1,7 @@
 package com.jaimemoro.cornermanbox.data.entities;
 
 import androidx.room.Entity;
+import androidx.room.Ignore;
 import androidx.room.PrimaryKey;
 
 @Entity(tableName = "entrenamientos")
@@ -12,4 +13,15 @@ public class Entrenamiento {
     public int puntosGanados;
     public int roundsCompletados;
     public int duracionTotalSegundos;
+
+    public Entrenamiento() {
+    }
+@Ignore
+    public Entrenamiento(int duracionTotalSegundos, long fecha, int id, int puntosGanados, int roundsCompletados) {
+        this.duracionTotalSegundos = duracionTotalSegundos;
+        this.fecha = fecha;
+        this.id = id;
+        this.puntosGanados = puntosGanados;
+        this.roundsCompletados = roundsCompletados;
+    }
 }
